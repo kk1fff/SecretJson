b16ecfa87c5f8247c2ae3668f1d50e644c16967b
kk1fff <p@m.patrickz.net>
2011-12-10 16:15:38 +0800

    1. Add first class of project. 2. Add apache commons codes package to 3rd party folder. 3. Move json to 3rd party.

---
+++ src/net/patrickz/onwork/secretjson/SecretJson.java
@@ -0,0 +1,11 @@
+package net.patrickz.onwork.secretjson;
+
+import javax.crypto.Cipher;
+
+import org.json.JSONObject;
+
+public class SecretJson extends JSONObject {
+	public SecretJson(String enc_string, Cipher cipher) {
+		
+	}
+}
